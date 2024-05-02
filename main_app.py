a079bf180144cd813e7d1e5370ce717ff48442cc
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 06:09:26 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,16 +1,35 @@
-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-df = pd.read_csv('car_data.csv')
-st.title('Car Data')
-st.write(df)
+# Uncomment to load data
+@st.cache_data  # This line caches the function result
+def load_data(file_path):
+    return pd.read_csv(file_path)
 
-# with st.sidebar:
-  # st.write("Here is sidebar")
+file_path = 'merged1.csv'  # Adjust the file path to your CSV file
+df = load_data(file_path)
 
-# Add a text input widget to the sidebar
-car_name = st.sidebar.text_input("Enter car name:")
-# Display a greeting message using the input from the sidebar
-#st.write(f"Input the Car Name: {user_input}!")
+# Sidebar widgets
+#start_year = st.sidebar.selectbox('Select Start Year', df['Year'].unique())
+#end_year = st.sidebar.selectbox('Select End Year', df['Year'].unique())
+#show_data = st.sidebar.checkbox('Show Data')
+
+# Main layout
+main_tab, research_questions, data_sources, data_vis = st.columns(4)
+tabs = st.radio('Navigation', ['Introduction', 'Research Questions', 'Data Sources', 'Data and visualizations'])
+
+if tabs == 'Introduction':
+    main_tab.write('This is the Introduction tab.')
+elif tabs == 'Research Questions':
+    research_questions.write('This is the Research Questions tab.')
+elif tabs == 'Data Sources':
+    data_sources.write('This is the Data Sources tab.')
+elif tabs == 'Data and visualizations':
+    data_vis.write('This is the Data Visualization tab.')
+    # Filter data based on user selection
+    #filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+
+    # Display filtered data if checkbox is selected
+    #if show_data:
+        #data_vis.subheader('Filtered Data')
+        #data_vis.write(filtered_df)
