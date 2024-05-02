ad5c12d3ada202adb859a644c3f0cb6c0bed909a
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 11:47:09 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -96,7 +96,7 @@ elif tabs == 'Data and visualizations':
     fig = px.scatter(
         df, 
         x="Year", 
-        y="CPI")
+        y="Return (%)")
     fig.show()
 
     tab1, tab2 = st.tabs(["Scatter Plot of American Tower Revenue from 2009 - 2021", "Graph Description"])
