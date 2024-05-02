ae1b3966f0eba37c7573f30481d1ce42710b2b7b
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 10:03:25 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st 
+import altair as alt
 
 @st.cache_data
 def load_data(file_path):
@@ -12,11 +13,15 @@ st.sidebar.info("DSCI 510 Final Project")
 
 # Main layout
 tabs = st.sidebar.radio('Navigation', ['Introduction', 'Research Questions', 'Data Sources', 'Data and visualizations'])
-st.markdown("My webapp contains the following four pages: Introduction, Research Questions, Dataframe, and Data and Visualizations.")
-st.markdown("\t     I. To navigate my webapp, begin at the Introduction! This page is an overview of takeaways from my plots, tables, and its interactivity as well as my conclusions from the visualizations. I also reflected on the project findings and process on this page.") 
-st.markdown("\t    II. The second page contains more in-depth reflection questions which have a few explanations as to what I discovered, challenges, skills I wish I had for the project, and future direction and steps.")
-st.markdown("\t   III. Next, on the Dataframe page, I listed all three sources and described how I acquired the datasets for my project, from web scraping the FRED website to utilizing the stock market and interest rate API’s.")
-st.markdown("\t    IV. The final page shows each visualization I’ve made and their interactivity from the datasets!")
+
+if tabs == 'Introduction':
+    st.title("Introduction")    
+    st.subheader("Name: Raksha Ravichandran")
+    st.markdown("My webapp contains the following four pages: Introduction, Research Questions, Dataframe, and Data and Visualizations.")
+    st.markdown("\t     I. To navigate my webapp, begin at the Introduction! This page is an overview of takeaways from my plots, tables, and its interactivity as well as my conclusions from the visualizations. I also reflected on the project findings and process on this page.") 
+    st.markdown("\t    II. The second page contains more in-depth reflection questions which have a few explanations as to what I discovered, challenges, skills I wish I had for the project, and future direction and steps.")
+    st.markdown("\t   III. Next, on the Dataframe page, I listed all three sources and described how I acquired the datasets for my project, from web scraping the FRED website to utilizing the stock market and interest rate API’s.")
+    st.markdown("\t    IV. The final page shows each visualization I’ve made and their interactivity from the datasets!")
     
 elif tabs == 'Research Questions':
     st.title("Research Questions")
@@ -42,3 +47,19 @@ elif tabs == 'Data and visualizations':
     if show_data:
         st.subheader('Filtered Data')
         st.write(filtered_df)
+
+    chart = alt.Chart(filtered_df).mark_circle().encode(
+        x='Year',
+        y='Interest Rate',
+        color='Origin',
+    ).interactive()
+
+    tab1, tab2 = st.columns(2)
+
+    with tab1:
+        # Use the Streamlit theme.
+        # This is the default. So you can also omit the theme argument.
+        st.altair_chart(chart, theme="streamlit", use_container_width=True)
+    with tab2:
+        # Use the native Altair theme.
+        st.altair_chart(chart, theme=None, use_container_width=True)
