f25a9355f5d19aa25db27ec425faa0f4c20ae64c
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 07:20:24 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,37 +1,36 @@
-import streamlit as st
-import pandas as pd
-
-# Uncomment to load data
-@st.cache_data # This line caches the function result
+@st.cache
 def load_data(file_path):
     return pd.read_csv(file_path)
 
 file_path = 'merged1.csv'  # Adjust the file path to your CSV file
 df = load_data(file_path)
 
-st.sidebar.info("Raksha's Data 510 Project: Stock Market, Interest Rates, and Real Estate Revenue During Economic Recessions")
+st.sidebar.info("DSCI 510 Final Project: ")
 
-# Sidebar widgets
-start_year = st.sidebar.selectbox('Select Start Year', df['Year'].unique())
-end_year = st.sidebar.selectbox('Select End Year', df['Year'].unique())
-show_data = st.sidebar.checkbox('Show Data')
+st.title("**Raksha Ravichandran**")
 
 # Main layout
-main_tab, research_questions, data_sources, data_vis = st.columns(4)
-tabs = st.radio('Navigation', ['Introduction', 'Research Questions', 'Data Sources', 'Data and visualizations'])
+tabs = st.sidebar.radio('Navigation', ['Introduction', 'Research Questions', 'Data Sources', 'Data and visualizations'])
 
 if tabs == 'Introduction':
-    main_tab.write('This is the Introduction tab.')
+    st.title("Introduction")
+    st.write('This is the Introduction tab.')
 elif tabs == 'Research Questions':
-    research_questions.write('This is the Research Questions tab.')
+    st.title("Research Questions")
+    st.write('This is the Research Questions tab.')
 elif tabs == 'Data Sources':
-    data_sources.write('This is the Data Sources tab.')
+    st.title("Data Sources")
+    st.write('This is the Data Sources tab.')
 elif tabs == 'Data and visualizations':
-    data_vis.write('This is the Data Visualization tab.')
+    st.title("Data and Visualizations")
+    st.write('This is the Data Visualization tab.')
+    start_year = st.sidebar.selectbox('Select Start Year', df['Year'].unique())
+    end_year = st.sidebar.selectbox('Select End Year', df['Year'].unique())
+    show_data = st.sidebar.checkbox('Show Data')
     # Filter data based on user selection
-    #filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+    filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
 
     # Display filtered data if checkbox is selected
-    #if show_data:
-        #data_vis.subheader('Filtered Data')
-        #data_vis.write(filtered_df)
+    if show_data:
+        st.subheader('Filtered Data')
+        st.write(filtered_df)
