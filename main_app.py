e4ac8a156856afde4ad448fc406e03f9c483e15a
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 09:58:58 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -12,11 +12,11 @@ st.sidebar.info("DSCI 510 Final Project")
 
 # Main layout
 tabs = st.sidebar.radio('Navigation', ['Introduction', 'Research Questions', 'Data Sources', 'Data and visualizations'])
-    st.markdown("My webapp contains the following four pages: Introduction, Research Questions, Dataframe, and Data and Visualizations.")
-    st.markdown("\t     I. To navigate my webapp, begin at the Introduction! This page is an overview of takeaways from my plots, tables, and its interactivity as well as my conclusions from the visualizations. I also reflected on the project findings and process on this page.") 
-    st.markdown("\t    II. The second page contains more in-depth reflection questions which have a few explanations as to what I discovered, challenges, skills I wish I had for the project, and future direction and steps.")
-    st.markdown("\t   III. Next, on the Dataframe page, I listed all three sources and described how I acquired the datasets for my project, from web scraping the FRED website to utilizing the stock market and interest rate API’s.")
-    st.markdown("\t    IV. The final page shows each visualization I’ve made and their interactivity from the datasets!")
+st.markdown("My webapp contains the following four pages: Introduction, Research Questions, Dataframe, and Data and Visualizations.")
+st.markdown("\t     I. To navigate my webapp, begin at the Introduction! This page is an overview of takeaways from my plots, tables, and its interactivity as well as my conclusions from the visualizations. I also reflected on the project findings and process on this page.") 
+st.markdown("\t    II. The second page contains more in-depth reflection questions which have a few explanations as to what I discovered, challenges, skills I wish I had for the project, and future direction and steps.")
+st.markdown("\t   III. Next, on the Dataframe page, I listed all three sources and described how I acquired the datasets for my project, from web scraping the FRED website to utilizing the stock market and interest rate API’s.")
+st.markdown("\t    IV. The final page shows each visualization I’ve made and their interactivity from the datasets!")
     
 elif tabs == 'Research Questions':
     st.title("Research Questions")
