7b4ff2cacbc999b67ee77476ec78aeef369d46c8
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-02 07:35:23 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -20,6 +20,7 @@ if tabs == 'Introduction':
     st.markdown("To navigate my webapp, begin at the Introduction! This page is an overview of takeaways from my plots, tables, and its interactivity as well as my conclusions from the visualizations. I also reflected on the project findings and process on this page.") 
     st.markdown("The second page contains more in-depth reflection questions which have a few explanations as to what I discovered, challenges, skills I wish I had for the project, and future direction and steps.")
     st.markdown("Next, on the Dataframe page, I listed all three sources and described how I acquired the datasets for my project, from web scraping the FRED website to utilizing the stock market and interest rate API’s.")
+    st.markdown("The final page shows each visualization I’ve made and their interactivity from the datasets!")
     
 elif tabs == 'Research Questions':
     st.title("Research Questions")
