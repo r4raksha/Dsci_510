d51cc69e8f064a81a9a3dac15f6d2dc3abd27758
r4raksha <166335856+r4raksha@users.noreply.github.com>
2024-05-03 08:41:38 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -124,10 +124,16 @@ elif tabs == 'Data and Visualizations':
                      color='CPI', 
                      title='Scatter Plot with Four Variables',
                      labels={'x': 'American Tower Real Estate Revenu', 'y': 'Stock Market Performance Return Rate Percentage'},  # Custom axis labels
-                 h    over_data={'size': True, 'color': True})  # Additional hover info
+                     over_data={'size': True, 'color': True})  # Additional hover info
 
     # Show plot
     fig.show()
+
+    tab1, tab2 = st.tabs(["Revenue", "Return Rate"])
+    with tab1:
+        st.plotly_chart(fig, use_container_width=True)
+    with tab2:
+        st.markdown("Graph Description")
 
 
     
